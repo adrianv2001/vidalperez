e5a1308f51ab01c82f95b90dc72565dc93308eca
Adrian Vidal <adrianvidalperez@dam.com>
2021-10-05 12:51:35 +0200

    Módulo Clientes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is a sample Python script.
-import sys
+import sys,var
 
+import events
 from window import *
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,8 +9,16 @@ from window import *
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        var.ui = Ui_MainWindow()
+        var.ui.setupUi(self)
+        '''
+        Eventos de boton
+        '''
+        var.ui.btnSalir.clicked.connect(events.Eventos.Salir)
+        '''
+        Eventos de la barra de menus
+        '''
+        var.ui.actionSalir.triggered.connect(events.Eventos.Salir)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
